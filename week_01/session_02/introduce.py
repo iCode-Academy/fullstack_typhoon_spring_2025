2c891d560d3ba27cbf4e6a852b499e2836dd1ca3
khangaikhuu <khangaikhuu@googlemail.com>
2025-03-27 17:53:58 +0800

    add: session 02

---
+++ week_01/session_02/introduce.py
@@ -0,0 +1,10 @@
+# introduce myself -- this is comment
+# introduce yourself in three lines
+# for example: your first name, lastname, age , gender
+# ,occupation and hobbies
+print("My Name is Khangaikhuu Uvgunkhuu")
+print("I'm 42 years old")
+print("I'm a man")
+print("I'm a software developer and iCode Academy Master Teacher")
+print("I like karate, swimming and running")
+print("I wrote it from Nano Editor")
